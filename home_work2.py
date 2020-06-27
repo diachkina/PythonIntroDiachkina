892bd69af734c0c4a0aa433cc185782844deb92b
diachkina <mezzanotik@gmail.com>
2020-06-28 01:13:38 +0300

    Done

---
+++ home_work2.py
@@ -0,0 +1,7 @@
+print("Escape sequences:")
+print("\\" "\\" "\t=> Backslash \\")
+print("\\" "'" "\t=> Single quote")
+print("\\" '"' "\t=> Double quote")
+print("\\" "t" "\t=> Horizontal Tab")
+print("\\" "b" "\t=> Backspace")
+print("\\" "n" "\t=> Linefid")
