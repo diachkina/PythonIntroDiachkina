fcea071d2e57de96403c2be976b9cdff4eb587a2
diachkina <mezzanotik@gmail.com>
2020-07-22 13:42:30 +0300

    +

--- Lesson4/home_work10.py
+++ Lesson4/home_work10.py
@@ -1,26 +1,30 @@
-n = int(input())
 kol = sUm = kol2 = kol1 = 0
-mAx = mIn = n
-while n != 0:
+mAx = mIn = 0
+while True:
+    n = int(input())
+    if n == 0:
+        break
+
+    if kol == 0:
+        mIn = n
+        mAx = n
     kol += 1
     sUm += n
-    n = int(input())
-    for i in range(1, n):
-        if n > mAx:
-            mAx = n
-        if n < mIn:
-            mIn = n
+
+    if n > mAx:
+        mAx = n
+    if n < mIn:
+        mIn = n
+
     if n % 2 == 0:
         kol2 += 1
     else:
         kol1 += 1
+
 print("Кол-во эл-в:", kol)
 print("Сумма:", sUm)
 print("Среднее арифметическое:", sUm / kol)
 print("Мах", mAx)
 print("Min:", mIn)
-print("Кол-во нечётных:", kol1)
 print("Кол-во чётных:", kol2)
-
-# при вводе одних нечётных не работает их кол-во.
-# их на 1 меньше, а в чётных 1.что я сделала неправильно?
+print("Кол-во нечётных:", kol1)
