1315de0cd0b415c44cd14e3b1b49fc290ad9988b
diachkina <mezzanotik@gmail.com>
2020-07-27 15:00:49 +0300

    +

--- Lesson6-7/home_work13.py
+++ Lesson6-7/home_work13.py
@@ -1,11 +1,10 @@
 from random import randint
 lst = [randint(1, 100) for _ in range(10)]
 print(lst)
-# lst.append(0)
-# print(lst)
+lst.append(0)
 k = int(input("Enter the index: "))
 c = int(input("Enter the value: "))
-# lst[k] = c
-for ind in range(k, len(lst)-1):
+for ind in range(len(lst) - 1, k, -1):
     lst[ind] = lst[ind - 1]
+lst[k] = c
 print(lst)
