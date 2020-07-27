67551518703d115bcbe3d7c5b919988e87c1152a
diachkina <mezzanotik@gmail.com>
2020-07-27 12:07:23 +0300

    +

--- Lesson6-7/home_work11.py
+++ Lesson6-7/home_work11.py
@@ -1,28 +1,28 @@
 h = int(input())
+w = h * 2 - 1
+
+for i in range(h):
+    print(i, end='\t')
+    for j in range(w):
+        if i == h - 1 or j == w // 2 + i or j == w // 2 - i:
+            print('* ', end='')
+        else:
+            print('  ', end='')
+    print()
+print()
+
+
+for i in range(h):
+    print(i, end='\t')
+    for j in range(w):
+        if i <= h - 1 and j <= w // 2 + i and j >= w // 2 - i:
+            print('* ', end='')
+        else:
+            print('  ', end='')
+    print()
+print()
+
 w = h
-
-for i in range(h):
-    print(i, end='\t')
-    for j in range(w):
-        if i == h // 2 or j == w // 2 + i or j == w // 2 - i:
-            print('* ', end='')
-        else:
-            print('  ', end='')
-    print()
-print()
-
-
-for i in range(h):
-    print(i, end='\t')
-    for j in range(w):
-        if i <= h // 2 and j <= w // 2 + i and j >= w // 2 - i:
-            print('* ', end='')
-        else:
-            print('  ', end='')
-    print()
-print()
-
-
 for i in range(h):
     print(i, end='\t')
     for j in range(w):
