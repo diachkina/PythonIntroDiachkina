bd4ddae8085ba59f7675902940ee7655200fe330
diachkina <mezzanotik@gmail.com>
2020-08-02 00:56:00 +0300

    +

---
+++ Lesson9/home_work22.py
@@ -0,0 +1,11 @@
+def season(c):
+    if 2 < c < 6:
+        print("spring")
+    elif 5 < c < 9:
+        print("summer")
+    elif 8 < c < 12:
+        print("autumn")
+    else:
+        print("winter")
+
+season(5)
