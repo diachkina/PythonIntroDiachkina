07397699bff4e4dbed6c0ba12c891a2aa7bfc736
diachkina <67434716+diachkina@users.noreply.github.com>
2020-08-02 11:20:07 +0300

    Update home_work1.py
    ++

--- Lesson2/home_work1.py
+++ Lesson2/home_work1.py
@@ -3,3 +3,21 @@ print("    *     *   * \t* *   *\t\t*\t*   *\n"
       "    *     * * * \t*  *  *\t\t*\t* * *\n"
       "    *     *   * \t*   * *\t\t*\t*   *\n"
       "    *     *   * \t*     *\t\t*\t*   *")
+
+
+text = input()
+text = text.split()
+quantity_words = {}
+for key_word in text:
+    quantity_words[key_word] = text.count(key_word)
+    # quantity_words[key_word] = quantity_words.get(key_word, 0) + 1
+print(quantity_words)
+
+max_value = max(list(quantity_words.values()))
+word = ''
+for key, value in quantity_words.items():
+    if value == max_value:
+        word = key
+
+
+print(word, max_value)
