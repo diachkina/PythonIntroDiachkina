86bd3ca7fd95ac5a3b60bc7bece34041cb54e795
diachkina <mezzanotik@gmail.com>
2020-08-16 22:29:34 +0300

    add_file

---
+++ Lesson11-12/matrix_1.py
@@ -0,0 +1,19 @@
+from random import randint
+
+rows = int(input())
+cols = int(input())
+
+matrix = [[randint(10, 99) for _ in range(cols)] for _ in range(rows)]
+
+total_cols = [0] * cols
+for i in range(rows):
+    total_rows = 0
+    for j in range(cols):
+        total_rows += matrix[i][j]
+        total_cols[j] += matrix[i][j]
+        print('{:<5}'.format(matrix[i][j]), end='')
+    print(" ", total_rows)
+print()
+
+for i in total_cols:
+    print('{:<5}'.format(i), end='')
