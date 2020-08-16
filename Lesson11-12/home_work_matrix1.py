3ad777055938236248a6243c0dfe4b955da84348
diachkina <67434716+diachkina@users.noreply.github.com>
2020-08-16 20:56:55 +0300

    Update home_work_matrix1.py

--- Lesson11-12/home_work_matrix1.py
+++ Lesson11-12/home_work_matrix1.py
@@ -5,26 +5,16 @@ cols = int(input())
 
 matrix = [[randint(10, 99) for _ in range(cols)] for _ in range(rows)]
 
-# print(matrix)
-# print()
-
+total_cols = [0] * cols
 for i in range(rows):
     total_rows = 0
     for j in range(cols):
         total_rows += matrix[i][j]
+        total_cols[j] += matrix[i][j]
         print('{:<5}'.format(matrix[i][j]), end='')
     print(" ", total_rows)
 print()
 
-total_cols = [0] * cols
-for row in range(len(matrix)):
-    for col in range(len(matrix[row])):
-        total_cols[col] += matrix[row][col]
-
-#print('{:>3}'.format(list(total_cols)), end='')
-print(total_cols, end='')
-
-# for line in rating:
-#     line = line.split(', ')[1: -2]
-#     for idx in range(len(line)):
-#         matrix[idx] += int(line[idx]
+for i in total_cols:
+    print('{:<5}'.format(i), end='')
+    
