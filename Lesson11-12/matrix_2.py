aaf4df1209ec8eadc922958256644fe0677fc773
diachkina <mezzanotik@gmail.com>
2020-08-23 09:04:06 +0300

    sdfhsdhdsfh

--- Lesson11-12/matrix_2.py
+++ Lesson11-12/matrix_2.py
@@ -3,42 +3,48 @@ from random import randint
 C = R = int(input("Enter the number more than 5: "))
 
 matrix = [[randint(1, 50) for _ in range(C)] for _ in range(R)]
-# print(matrix)
+print(matrix)
 
 total_C = [0] * C
 for i in range(R):
     # total_R = 0
     for j in range(C):
-        # total_R += matrix[i][j]
+        # total_R += matrix[j][i]
         total_C[j] += matrix[i][j]
         print('{:<5}'.format(matrix[i][j]), end='')
+    # print(" ", total_R)
     print()
-
 for i in total_C:
     print('{:<5}'.format(i), end='')
 
 print()
 
-
-def bubble_sort(array):
-    for i in range(len(array) - 1):
-        flag = True
-        for j in range(len(array) - 1 - i):
-            if array[j] > array[j + 1]:
-                array[j], array[j + 1] = array[j + 1], array[j]
-                flag = False
-
+for j in range(len(matrix) - 1):
+    flag = True
+    for i in range(len(matrix) -1-j):
+        if sum(matrix[j])>sum(matrix[j+1]):
+            matrix[j], matrix[j+1] = matrix[j+1], matrix[j]
+            flag = False
         if flag:
             break
     print()
+for i in range(C):
+    for j in range(R):
+        print('{:<5}'.format(matrix[i][j]), end='')
+    print()
 
 
-bubble_sort(total_C)
-print(total_C)
 
-# for i in range(R):
-#     for j in range(C):
-#         print('{:<5}'.format(matrix[i][j]), end='')
+
+# print()
+
+#
+# bubble_sort(matrix)
+# print(matrix)
+
+# for i in range(C):
+#     for j in range(R):
+#         print('{:<5}'.format(matrix[j][i]), end='')
 #     print()
 
-
+# print(matrix)
