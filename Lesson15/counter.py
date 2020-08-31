574a20c5a4cc279229617d5613c0112bbeb4453c
diachkina <mezzanotik@gmail.com>
2020-08-31 10:24:00 +0300

    +

--- Lesson15/counter.py
+++ Lesson15/counter.py
@@ -1,19 +1,20 @@
 class Counter:
-    current = 0
 
     def __init__(self, minimum, maximum):
         self.minimum = minimum
         self.maximum = maximum
+        self.current = self.minimum
 
     def count(self):
         if self.current < self.maximum:
             self.current += 1
             return self.current
         else:
-            return self.minimum
+            return 'Out of range'
 
 
-res = Counter(0, 5)
+res = Counter(24, 30)
+print(res.count())
 print(res.count())
 print(res.count())
 print(res.count())
